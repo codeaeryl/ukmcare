1bfa3fd8e94da8606a9fd20774e602af5eddc251
Jayden ME <codeaeryl@gmail.com>
2026-05-09 08:45:15 +0700

    created Patient table model

---
+++ app/Models/Patient.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Gender;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Patient extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'mrn';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'mrn',
+        'nik',
+        'full_name',
+        'pob',
+        'dob',
+        'gender',
+        'address',
+        'phone',
+        'blood_type',
+        'bpjs_number',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'dob' => 'date',
+        'gender' => Gender::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}

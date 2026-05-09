34c6f75b1af8af65e93f4588d2f3b99e9c86c696
Jayden ME <codeaeryl@gmail.com>
2026-05-09 09:53:15 +0700

    fixed inconsistency in medicine's price with others

--- database/migrations/2026_05_09_000005_create_medicines_table.php
+++ database/migrations/2026_05_09_000005_create_medicines_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('medicines', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('stock')->default(0);
-            $table->decimal('price', 10, 2);
+            $table->decimal('price', 12, 2);
             $table->timestamps();
         });
     }

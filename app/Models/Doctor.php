4784a697e094496c1092cad6eba9e2c7b477121c
Jayden ME <codeaeryl@gmail.com>
2026-05-09 08:48:23 +0700

    created Doctor table model, migration and seeder

---
+++ app/Models/Doctor.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Doctor extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'doctor_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'doctor_id',
+        'nik',
+        'sip',
+        'str',
+        'full_name',
+        'specialist',
+        'phone',
+        'is_bpjs',
+        'is_active',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'is_bpjs' => 'boolean',
+        'is_active' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
